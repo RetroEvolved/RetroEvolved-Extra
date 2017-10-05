8508c39475238715a21dbcb33fc51a1753914800
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-04 23:28:56 -0700

    Update install-extras.sh

--- install-extras.sh
+++ install-extras.sh
@@ -2,4 +2,3 @@
 
 cp -R scriptmodules/ports/* "/home/pi/RetroEvolved-Setup/scriptmodules/ports/"
 cp -R scriptmodules/emulators/* "/home/pi/RetroEvolved-Setup/scriptmodules/emulators"
-cp -R scriptmodules/supplementary/* "/home/pi/RetroEvolved-Setup/scriptmodules/supplementary"
