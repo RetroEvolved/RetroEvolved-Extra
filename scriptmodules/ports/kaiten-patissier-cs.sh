7f46c8e4778fed87766e202437acd49af168ecd2
RetroGameClub <32254569+RetroGameClub@users.noreply.github.com>
2017-10-04 20:36:00 -0700

    Update kaiten-patissier-cs.sh

--- scriptmodules/ports/kaiten-patissier-cs.sh
+++ scriptmodules/ports/kaiten-patissier-cs.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-# This file is part of The RetroPie Project
+# This file is part of The RetroEvolved Project
 # 
-# The RetroPie Project is the legal property of its developers, whose names are
+# The RetroEvolved project is a reworking of the RetroPie Project which is the legal property of its developers, whose names are
 # too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
 # 
 # See the LICENSE.md file at the top-level directory of this distribution and 
-# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+# at https://raw.githubusercontent.com/RetroEvolved/RetroEvolved-Setup/master/LICENSE.md
 #
 
 rp_module_id="kaiten-patissier-cs"
